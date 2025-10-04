0235ad6675500d25f4f7f1f77474ae2e66cc769f
Istheaksayem <istheakahmed9345@gmail.com>
2025-10-04 11:54:47 +0600

    added users,users2

---
+++ src/components/Users2/Users2.jsx
@@ -0,0 +1,14 @@
+import React, { use } from 'react';
+
+const Users2 = ({users2Promise}) => {
+
+    const users = use(users2Promise);
+    console.log('users to suspanse data',users)
+    return (
+        <div>
+            <h2>This is Users2</h2>
+        </div>
+    );
+};
+
+export default Users2;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -10,6 +10,13 @@ import Root from './components/Root/Root.jsx';
 import Home from './components/Home/Home.jsx';
 import Mobiles from './components/Mobiles/Mobiles.jsx';
 import Laptops from './components/Laptops/Laptops.jsx';
+import Users from './components/Users/Users.jsx';
+import Users2 from './components/Users2/Users2.jsx';
+
+
+
+const users2Promise = fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json());
 
 const router = createBrowserRouter([
 
@@ -19,7 +26,20 @@ const router = createBrowserRouter([
     children: [
       { index: true, Component: Home },
       { path: 'mobiles', Component: Mobiles },
-      { path: 'laptops', Component: Laptops }
+      { path: 'laptops', Component: Laptops },
+      {
+         path: 'users', 
+         loader:() => fetch('https://jsonplaceholder.typicode.com/users'),
+         Component: Users 
+        },
+        {
+          path:'users2',
+          element:<Suspense fallback={<span>loading....</span>}>
+            <Users2 users2Promise={users2Promise}></Users2>
+          </Suspense>
+
+
+        }
     ]
   },
 
